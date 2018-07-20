3e61f7bc765fb11467ce5c57726e0a8a0b8a7a35
Erin Hogan <hogan10@charter.net>
2018-07-19 23:15:00 -0500

    Homework Week 2 Part 3

---
+++ CG-2-3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CG_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your first name");
+            string myFirstName = Console.ReadLine();
+
+            Console.Write("Enter your birth month");
+            string myBirthMonth = Console.ReadLine();
+
+            Console.Write("Enter your birth day");
+            string myBirthDay = Console.ReadLine();
+
+            Console.WriteLine("Here is your name and birthday:" + " " + myFirstName + myBirthMonth + myBirthDay);
+            Console.ReadLine();
+        }
+    }
+}
